2a17412336a825cc180f2dabd1265f24ab32c23d
ahmadzikrillah <120207553+ahmadzikrillah@users.noreply.github.com>
2025-04-30 03:14:55 +0700

    Update datasheet.js
    .

--- datasheet.js
+++ datasheet.js
@@ -1,13 +1,1 @@
-// Fungsi untuk memuat dan membaca file JSON
-fetch('datasheet.json')
-  .then(response => response.json())
-  .then(data => {
-    console.log("Data berhasil dimuat:", data);
-
-    // Contoh: Menampilkan pertanyaan pertama dalam JSON
-    let pertanyaanPertama = data[0].Pertanyaan;
-    console.log("Pertanyaan pertama:", pertanyaanPertama);
-  })
-  .catch(error => {
-    console.error("Error dalam memuat data:", error);
-  });
+.
